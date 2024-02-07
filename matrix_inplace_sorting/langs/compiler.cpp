bffc499075f9c90e13a23ab3d0116997ef0cad0b
Prasandh <prasandh@multicorewareinc.com>
2024-02-07 16:08:24 +0530

    Update user & compiler boilerplates format

--- matrix_inplace_sorting/langs/compiler.cpp
+++ matrix_inplace_sorting/langs/compiler.cpp
@@ -1,11 +1,3 @@
-#include <iostream>
-#include <string>
-#include <bits/stdc++.h>
-
-void sortMatrix(int m, int n, std::vector<std::vector<int>>& input)
-{
-    //Write your code here
-}
 int main()
 {
     int m;
---
+++ matrix_inplace_sorting/langs/user.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+void sortMatrix(int m, int n, std::vector<std::vector<int>>& input)
+{
+    //Write your code here
+}
