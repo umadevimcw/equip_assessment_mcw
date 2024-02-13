7ebe70c41f2b9cdba06c27fed568a21e500da592
RahulSundarMCW <rahul.sundar@multicorewareinc.com>
2024-02-13 13:47:26 +0530

    added land for aggregateWeightsByValue

---
+++ level_2/aggregateWeightsByValue/langs/compiler.cpp
@@ -0,0 +1,27 @@
+int main() 
+{
+    int m, n;
+    cin >> m;
+    int set1[m][2];
+    for (int i = 0; i < m; i++) 
+    {
+        cin >> set1[i][0];
+        cin >> set1[i][1];
+    }
+    cin >> n;
+    int set2[n][2];
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> set2[i][0];
+        cin >> set2[i][1];
+    }
+
+    int resultSize;
+    int** result = aggregateWeightsByValue(m, set1, n, set2, resultSize);
+
+    for (int i = 0; i < resultSize; ++i)
+        cout << result[i][0] << " " << result[i][1] << endl;
+
+
+    return 0;
+}
---
+++ level_2/aggregateWeightsByValue/langs/user.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int** aggregateWeightsByValue(int M, int set1[][2], int N, int set2[][2], int& resultSize)
+ {
+    //Write your code here
+ }
