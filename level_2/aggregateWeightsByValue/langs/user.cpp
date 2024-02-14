14154c3340039f43f01ca22ee1c66432085740ac
RahulSundarMCW <rahul.sundar@multicorewareinc.com>
2024-02-14 10:28:51 +0530

    modified user.c in aggregateWeightsBValue

--- level_2/aggregateWeightsByValue/langs/user.cpp
+++ level_2/aggregateWeightsByValue/langs/user.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int** aggregateWeightsByValue(int M, int set1[][2], int N, int set2[][2], int& resultSize)
+int** aggregateWeightsByValue(int M, int set1[][2], int N, int set2[][2], int *resultSize)
  {
     //Write your code here
  }
