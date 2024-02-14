83f8cd4b36fc0f67be24acc067fa2277557ecceb
RahulSundarMCW <rahul.sundar@multicorewareinc.com>
2024-02-14 10:25:09 +0530

    included header in user.c  in find_num_Subtree_Structure

--- level_2/find_num_Subtree_Structure/langs/user.c
+++ level_2/find_num_Subtree_Structure/langs/user.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+    int value;
+    struct Node* next;
+} Node;
+
+typedef struct Tree 
+{
+    int node;
+    Node* neighbors;
+} Tree;
+
 void find_num_Subtree_Structure(int N, int R, Tree* input_tree, int* output_subtree_count) 
 {
     //Write your code here
