007915af872dd424f2e32c3aa8dead74d7037731
RahulSundarMCW <rahul.sundar@multicorewareinc.com>
2024-02-14 18:35:50 +0530

    modify code alignment of narrative4

--- level_2/narrative4_frequent_integer_not_blocked/langs/compiler.py
+++ level_2/narrative4_frequent_integer_not_blocked/langs/compiler.py
@@ -1,10 +1,9 @@
 def main():
     n = int(input())
-arr = list(map(int, input().split()))
-m = int(input())
-b_arr = list(map(int, input().split()))
-
-print(frequent_integer_not_blocked(n, arr, m, b_arr))
+    arr = list(map(int, input().split()))
+    m = int(input())
+    b_arr = list(map(int, input().split()))
+    print(frequent_integer_not_blocked(n, arr, m, b_arr))
 
 if __name__ == "__main__":
     main()
