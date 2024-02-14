8ee9f90cf1922f1c8cf979056de7a01fbf6783ff
RahulSundarMCW <rahul.sundar@multicorewareinc.com>
2024-02-14 17:57:38 +0530

    modify code alignment of langs in easy5

--- level_2/easy5_find_pairs/langs/compiler.java
+++ level_2/easy5_find_pairs/langs/compiler.java
@@ -1,27 +1,26 @@
 public static void main(String[] args) 
-    {
-        Scanner scanner = new Scanner(System.in);
+{
+    Scanner scanner = new Scanner(System.in);
     int R = scanner.nextInt();
-        int N = scanner.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) 
-        {
-            arr[i] = scanner.nextInt();
-        }
-     
-        List<int[]> pairs = new ArrayList<>();
-        find_pairs(arr, N, R, pairs);
-        if (pairs.isEmpty())
-        {
-            System.out.println("No such pair");
-        } 
-        else 
-        {
-            for (int[] pair : pairs)
-            {
-                System.out.println("(" + pair[0] + ", " + pair[1] + ")");
-            }
-        }
-        scanner.close();
+    int N = scanner.nextInt();
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++) 
+    {
+        arr[i] = scanner.nextInt();
     }
+    List<int[]> pairs = new ArrayList<>();
+    find_pairs(arr, N, R, pairs);
+    if (pairs.isEmpty())
+    {
+        System.out.println("No such pair");
+    } 
+    else 
+    {
+        for (int[] pair : pairs)
+        {
+            System.out.println("(" + pair[0] + ", " + pair[1] + ")");
+        }
+    }
+    scanner.close();
+}
 
--- level_2/easy5_find_pairs/langs/compiler.py
+++ level_2/easy5_find_pairs/langs/compiler.py
@@ -1,15 +1,15 @@
 def main():
   R = int(input())
-    N = int(input())
-    arr = list(map(int, input().split()))
-    pairs = []
+  N = int(input())
+  arr = list(map(int, input().split()))
+  pairs = []
 
-    find_pairs(arr,N, R, pairs)
+  find_pairs(arr,N, R, pairs)
 
-    if not pairs:
-        print("No such pair")
-    else:
-        print(pairs)
+  if not pairs:
+     print("No such pair")
+  else:
+     print(pairs)
 
 
 if __name__ == "__main__":
