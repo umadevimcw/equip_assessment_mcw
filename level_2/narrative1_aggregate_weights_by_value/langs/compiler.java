f6a06e19240dddaf13174a5ce91bfea6f7ad5239
RahulSundarMCW <159773017+RahulSundarMCW@users.noreply.github.com>
2024-02-20 11:33:34 +0000

    add compiler.java in narrative1

---
+++ level_2/narrative1_aggregate_weights_by_value/langs/compiler.java
@@ -0,0 +1,25 @@
+public static void main(String[] args) 
+{
+    Scanner scanner = new Scanner(System.in);
+
+    int m = scanner.nextInt();
+    int[][] set1 = new int[m][2];
+    for (int i = 0; i < m; i++) 
+    {
+        set1[i][0] = scanner.nextInt();
+        set1[i][1] = scanner.nextInt();
+    }
+
+    int n = scanner.nextInt();
+    int[][] set2 = new int[n][2];
+    for (int i = 0; i < n; i++) 
+    {
+        set2[i][0] = scanner.nextInt();
+        set2[i][1] = scanner.nextInt();
+    }
+
+    int[][] result = aggregateWeightsByValue(set1, set2);
+
+    for (int[] pair : result) 
+        System.out.println(pair[0] + " " + pair[1]);
+}
