c3ce3cb374809011531c07178813f028735cb63e
RahulSundarMCW <159773017+RahulSundarMCW@users.noreply.github.com>
2024-02-22 10:38:20 +0000

    Modify compiler.py to correct the print format

--- level_2/easy5_find_pairs/langs/compiler.py
+++ level_2/easy5_find_pairs/langs/compiler.py
@@ -1,15 +1,16 @@
 def main():
-  R = int(input())
-  N = int(input())
-  arr = list(map(int, input().split()))
-  pairs = []
+    R = int(input())
+    N = int(input())
+    arr = list(map(int, input().split()))
+    pairs = []
 
-  find_pairs(arr,N, R, pairs)
+    find_pairs(arr, N, R, pairs)
 
-  if not pairs:
-     print("No such pair")
-  else:
-     print(pairs)
+    if not pairs:
+        print("No such pair")
+    else:
+        for pair in pairs:
+            print(pair)
 
 
 if __name__ == "__main__":
